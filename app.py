f57e5eed363e5881dd13ed4f4625b24e66bb0ab6
Rahma Ahmed <rahmaahmedschool@gmail.com>
2017-04-08 16:44:27 -0400

    bug fix

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask
+
+import os, json, re
+import flask
+
+from flask import (
+	Flask, 
+	render_template, 
+	redirect, 
+	url_for, 
+	request, 
+	make_response,
+)
+
 app = Flask(__name__)
-@app.route("/")
+
+@app.route('/')
 def main():
-    return "Welcome!"
+	return render_template("Hangman.html")
+
+@app.route('/game', methods=['GET', 'POST'])
+def game():
+	 words = ['red', 'blue', 'green']
+	 random_word = (random.choice(words))
+	 return render_template("game.html",random_word)
+
 if __name__ == "__main__":
-    app.run()
+	app.run()
