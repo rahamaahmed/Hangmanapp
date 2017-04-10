e0043a73336165552bda25ac472e67470ada2bfb
Rahma Ahmed <rahmaahmedschool@gmail.com>
2017-04-09 23:47:24 -0400

    Hangman MVP

--- app.py
+++ app.py
@@ -9,24 +9,57 @@ from flask import (
 	redirect, 
 	url_for, 
 	request, 
+	session,
 	make_response,
 )
 
 app = Flask(__name__)
+
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
 	if request.method == 'GET':  
 		return render_template("Hangman.html")
 	else:
-		return redirect(url_for('game'))
+		return redirect(url_for('setup'))
 
-@app.route('/game', methods=['GET', 'POST'])
-def game():
+@app.route('/setup', methods=['GET','POST'])
+def setup():
 	words = ['red', 'blue', 'green']
-	random_word = (random.choice(words))
-	return render_template("game.html",random_word=random_word)
+	session['random_word'] = (random.choice(words))
+	session['Word_length'] = len(session['random_word'])
+	session['blank_word'] = "_ " * session['Word_length']
+	session['blank_list'] = session['blank_word'].split()
+	session['Word_list']= list(session['random_word'])  
+	return render_template("setup.html",random_word=session['random_word'],
+		Word_list=session['Word_list'],blank_list=session['blank_list'])
 	
+
+@app.route('/game', methods=['GET','POST'])
+def game():
+	if request.method == 'POST': 
+		session['textbox']=request.form['text']
+		#print(session['textbox'])
+		Word_list=session.get('Word_list',None)
+		blank_list=session.get('blank_list',None)
+		letter_choice=session.get('textbox',None)
+		for i, j in enumerate(Word_list):
+			if j==letter_choice:
+				blank_list[i]=letter_choice
+		return render_template("game.html",blank_list=session['blank_list'])
+	#textbox = session.get('textbox', None)
+	#return render_template("game.html",textbox=textbox)
 
 if __name__ == "__main__":
 	app.run(debug=True)
+
+"""
+one char lowercase tell if wrong
+limited chances and visual image
+get words from word api
+choose diffculty
+topic
+try again
+you won/lost
+deploy app to heroku"""
