1e5492bb4f51ed885245e3f59399e8103a5a1649
Rahma Ahmed <rahmaahmedschool@gmail.com>
2017-05-06 18:50:01 -0400

    Finished difficulty levels

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-
+from __future__ import print_function
 import os, json, re
 import flask
 import random
+import sys
 
 from flask import (
 	Flask, 
@@ -17,21 +18,37 @@ app = Flask(__name__)
 
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
+def get_word(words,difficulty_level):
+	while True:
+		temp_random_word =(random.choice(words)).lower()
+		if difficulty_level=='Normal' and len(set(temp_random_word))<=7:
+			session['random_word']=temp_random_word
+			break
+		elif difficulty_level=='Hard' and len(set(temp_random_word))>7:
+			session['random_word'] = temp_random_word
+			break
+
+
 @app.route('/', methods=['GET', 'POST'])
 def main():
 	if request.method == 'GET':  
 		return render_template("Hangman.html")
 	else:
-		return redirect(url_for('setup'))
+		if request.form['difficulty'] == 'Normal':
+			session['difficulty_level']=request.form['difficulty']
+			return redirect(url_for('main'))
+		if request.form['difficulty'] == 'Hard':
+			session['difficulty_level']=request.form['difficulty']
+			return redirect(url_for('main'))
+		if request.form['form1']== 'Submit':
+			return redirect(url_for('setup'))
 
 @app.route('/setup', methods=['GET','POST'])
 def setup():
 	text_file = open('Hardwords.txt',"r")
 	words = text_file.read().split(',')
-	session['random_word'] = random.choice(words).lower()
+	get_word(words,session['difficulty_level'])
 	text_file.close()
-	#words = ['red', 'blue', 'green' , 'abcdefghabci']
-	#session['random_word'] = (random.choice(words))
 	session['Word_length'] = len(session['random_word'])
 	session['blank_word'] = "_ " * session['Word_length']
 	session['blank_list'] = session['blank_word'].split()
@@ -50,6 +67,7 @@ def game():
 			blank_list=session.get('blank_list',None)
 			letter_choice=session.get('textbox',None)
 			letter_choice=letter_choice.lower()
+			#cross letter from list
 			if re.match("^[a-z]*$", letter_choice) and len(letter_choice) == 1:
 				for i, j in enumerate(Word_list):
 					if j==letter_choice:
@@ -63,8 +81,6 @@ def game():
 				error="Error! Only letters a-z and 1 characters allowed!, try again"
 				session['Counter'] = session['Counter'] + 1
 				return render_template("game.html",blank_list=session['blank_list'],error=error)
-	#textbox = session.get('textbox', None)
-	#return render_template("game.html",textbox=textbox)
 		else:
 			session['game_status'] = 'lost'
 			return redirect(url_for('done'))
@@ -78,8 +94,9 @@ if __name__ == "__main__":
 	app.run(debug=True)
 
 """
-visual image
-get words from word api
-choose diffculty
-topic
-deploy app to heroku"""
+check the try agian
+cross letters from a-z
+visual hangman image
+make the site look good
+api/topic/hint
+"""
